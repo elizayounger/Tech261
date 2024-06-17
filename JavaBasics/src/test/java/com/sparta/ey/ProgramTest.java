909e71ee82540fa6935460e6935c6d419a94dbee
elizayounger29 <eyoungergithub@hotmail.com>
2024-06-17 15:10:07 +0100

    added JUnit and created simple test

---
+++ JavaBasics/src/test/java/com/sparta/ey/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.sparta.ey;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+
+    @Test
+    public void basicTest() {
+        Assertions.assertTrue(5 == 5);
+    }
+}
